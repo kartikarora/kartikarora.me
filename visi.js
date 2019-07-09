5e14d1f28afa8b845b5337dc0e2db1528533b026
Kartik Arora <chipset95@yahoo.co.in>
2019-07-09 19:21:44 +1000

    random graphic visualizer

---
+++ visi.js
@@ -0,0 +1,27 @@
+let mic, fft;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    colorMode(HSL);
+    noStroke();
+    mic = new p5.AudioIn();
+    mic.start();
+    fft = new p5.FFT();
+    fft.setInput(mic);
+}
+
+function draw() {
+    background(0);
+    translate(0, height / 2);
+    let vals = fft.analyze();
+    for (let i = 0; i < vals.length - 800; i++) {
+        let xPos = map(i, 0, vals.length - 800, 0, width);
+        let y1Pos = map(vals[i], 0, 255, 0, height / 2);
+        let y2Pos = map(vals[i], 0, 255, 0, -height / 2);
+        let size = map(vals[i], 0, 255, 0, 30);
+        let clr = color(map(vals[i], 0, 255, 200, 255));
+        fill(clr, clr, clr);
+        circle(xPos, y1Pos, size);
+        circle(xPos, y2Pos, size);
+    }
+}
