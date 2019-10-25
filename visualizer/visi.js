8e2d3f31e24ace3a814e8b8b09fae54af2fea703
Kartik Arora <chipset95@yahoo.co.in>
2019-10-25 15:32:06 +1100

    symmteric visualizer

--- visualizer/visi.js
+++ visualizer/visi.js
@@ -15,7 +15,7 @@ function draw() {
     translate(0, height / 2);
     let vals = fft.analyze();
     for (let i = 0; i < vals.length - 800; i++) {
-        let xPos = map(i, 0, vals.length - 800, 0, width);
+        let xPos = map(i, 0, vals.length - 800, 0, width / 2);
         let y1Pos = map(vals[i], 0, 255, 0, height / 2);
         let y2Pos = map(vals[i], 0, 255, 0, -height / 2);
         let size = map(vals[i], 0, 255, 0, 30);
@@ -23,6 +23,8 @@ function draw() {
         fill(clr, clr, clr);
         circle(xPos, y1Pos, size);
         circle(xPos, y2Pos, size);
+        circle(width - xPos, y1Pos, size);
+        circle(width - xPos, y2Pos, size);
     }
 }
 
